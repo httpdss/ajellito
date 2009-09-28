843d4c9f3e735b56da595415c893863a96b97277
Emiliano.Heyns <Emiliano.Heyns@573662cc-8f25-11dd-82cb-3113a4e48fd9>
2009-09-28 09:31:30 +0000

    add task-owner: none tag to tag cloud

--- agilito/views.py
+++ agilito/views.py
@@ -1063,11 +1063,15 @@ def iteration_status(request, project_id, iteration_id=None, template='iteration
         for task in tasks:
             us = 'us-%d' % task.user_story.id
             ta = 'ta-%d' % task.id
+
             if task.owner:
                 tag = 'owner:' + task.owner.username
-                tags[tag].append(ta)
-                if not us in tags[tag]:
-                    tags[tag].append(us)
+            else:
+                tag = 'owner:none'
+            tags[tag].append(ta)
+            if not us in tags[tag]:
+                tags[tag].append(us)
+
             for tag in parse_tag_input(task.tags):
                 # tag = tag.name
                 if not us in tags[tag]:
