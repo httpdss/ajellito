9c11c198c3bde979947ce67f5a80a49d0cef1b5e
friflaj <friflaj@ajellito.com>
2009-12-09 20:49:34 +0100

    Hours tab generated wrong urls for sprint selection

--- agilito/urls.py
+++ agilito/urls.py
@@ -47,7 +47,7 @@ urlpatterns = patterns('agilito.views',
     url(r'^(?P<project_id>\d+)/iteration/(?P<iteration_id>\d+)/userstory/add/$', 'userstory_create', name="story_from_iteration"),
     
     url(r'^(?P<project_id>\d+)/iteration/hours/$', 'iteration_hours', name="current_iteration_hours"),
-    url(r'^(?P<project_id>\d+)/iteration/(?P<iteration_id>\d+)/hours/$', 'iteration_hours', name="itertion_hours_with_id"),
+    url(r'^(?P<project_id>\d+)/iteration/(?P<iteration_id>\d+)/hours/$', 'iteration_hours', name="iteration_hours_with_id"),
     (r'^(?P<project_id>\d+)/iteration/(?P<iteration_id>\d+)/hours_export/$', 'hours_export'),
     (r'^(?P<project_id>\d+)/iteration/(?P<iteration_id>\d+)/cards/$', 'iteration_cards'),
     (r'^(?P<project_id>\d+)/iteration/(?P<iteration_id>\d+)/status_table/$', 'iteration_status_table'),
