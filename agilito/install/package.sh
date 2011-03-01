e462afa68ce8314d80171d801fbf53322a0e99d4
Kenneth Belitzky <kenny@belitzky.com>
2011-03-01 19:00:35 -0200

    removed unused files

--- agilito/install/package.sh
+++
@@ -1,6 +0,0 @@
-rm -rf dist
-git clone git://github.com/friflaj/ajellito.git dist
-cd dist
-python agilito/install.py --install=auto --nosqldiff
-python manage.py syncdb
-cd ..
--- agilito/install/windows/run.bat
+++
@@ -1,1 +0,0 @@
-python\python.exe manage.py runserver
