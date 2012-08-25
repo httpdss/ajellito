53bf00676d15c94ce8bd46284f9a1cb4544f9672
Kenneth Belitzky <kenny@belitzky.com>
2012-08-25 18:59:22 -0300

    Initial import of kanban features

---
+++ sprints/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import patterns, url
+
+urlpatterns = patterns('',
+    url(r'^graphs/$', 'sprints.views.index', name="sprints-index"),
+    url(r'^graphs/(?P<sprint_id>\d+)/detail/$', 'sprints.views.detail', name="sprints-detail"),
+    url(r'^(?P<sprint_id>\d+)/browse/$', 'sprints.views.browse', name="sprints-browse"),
+)
---
+++ tickets/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import patterns, url
+
+urlpatterns = patterns('',
+    url(r'^$', 'tickets.views.index', name="tickets-index"),
+    url(r'^tododevel/(?P<ticket_id>\d+)/$', 'tickets.views.movetododevel', name="tickets-movetododevel"),
+    url(r'^develdone/(?P<ticket_id>\d+)/$', 'tickets.views.movedeveldone', name="tickets-movedeveldone"),
+    url(r'^addticket/$', 'tickets.views.addticket', name="tickets-addticket"),
+    url(r'^ticketdetail/(?P<ticket_id>\d+)/$', 'tickets.views.detail', name="tickets-detail"),
+    url(r'^ticketedit/$', 'tickets.views.edit', name="tickets-edit"),
+    url(r'^ticketremove/(?P<ticket_id>\d+)/$', 'tickets.views.delete', name="tickets-delete"),
+)
